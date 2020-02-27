65bb959d792f504f979a91a7d12ca6e2f1f8562f
ronanfariaa <51864256+ronanfariaa@users.noreply.github.com>
2020-02-27 13:49:39 -0300

    Colocando Css no MenuSuperior

--- src/components/MenuSuperior/MenuSuperior.js
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logotop from "../../assets/img/logotop.png"
+import '../MenuSuperior/MenuSuperior.css'
 
 class MenuSuperior extends Component {
     render() {
         return (
             <div className=" container">
-                <a class="align-items-center " href="/">
-                    <center><img src={logotop} width="330" height="200" alt="logotop"></img></center>
+                <a className="menuSup_Href" href="/">
+                    <img className="menuSup-Logo" src={logotop} width="330" height="200" alt="logotop"></img>
                 </a>     <br></br>
-                <ul className="nav justify-content-center   ">
+                <ul className="menuSup-nav nav">
                     <li>
                         <Link to="/">   <input type="button" value="Planetas" class="btn btn-outline-warning " /> </Link>
                     </li>
